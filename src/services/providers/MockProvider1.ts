b230e27a260a88051baad49071a1ee54fabd7b6f
ayush1203kr <sahilayush1203@gmail.com>
2025-07-10 16:24:18 +0530

    Update MockProvider1.ts

--- src/services/providers/MockProvider1.ts
+++ src/services/providers/MockProvider1.ts
@@ -1,6 +1,6 @@
 export class MockProvider1 {
   async send(email: { id: string; to: string; subject: string; body: string }): Promise<void> {
-    // Simulate 70% chance of success
+ 
     const isSuccess = Math.random() < 0.7;
 
     if (!isSuccess) {
